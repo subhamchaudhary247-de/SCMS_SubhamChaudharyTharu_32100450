2151157a741018810596897b95f3aa9b9735059a
SUMIT <sc259042@gmail.com>
2026-03-28 16:47:38 +0800

    Creation of Student Class with their attributes of different data types and constructor to initiallize them.

--- src/main/java/scms/model/Student.java
+++ src/main/java/scms/model/Student.java
@@ -9,5 +9,34 @@ package scms.model;
  * @author sumitchaudhary
  */
 public class Student {
+    int studentID;
+    String name;
+    private int age;
+    String major;
+    int semester;
+    protected double percentageOfMarks;
+    protected double attendancePercentage;
     
+    public Student (int studentID,String name, int age,String major,int semester,double percentageOfMarks,
+     double attendancePercentage){
+        this.studentID = studentID;
+        this.age = age;
+        this.name = name;
+        this.major = major;
+        this.semester = semester;
+        this.attendancePercentage = attendancePercentage;
+        this.percentageOfMarks = percentageOfMarks;
+        
+    }
+    public double getpercentageOfMarks(){
+        return percentageOfMarks;
+    }
+    public void setpercentageOfMarks(double percentageOfMarks){
+        this.percentageOfMarks = percentageOfMarks;
+    }
+    public String toString(){
+        return "Student [ ID = " + studentID +",Name = " + name + ",Major = " + 
+                major + ",Semester = " + semester + ",AttendancePercentage = " + 
+                attendancePercentage + ",PercentageOfMarks = " + percentageOfMarks + "]";
+    }
 }

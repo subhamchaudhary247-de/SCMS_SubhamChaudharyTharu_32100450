8280514651fd9bd64dadfc6861e78d42b7d353c6
SUMIT <sc259042@gmail.com>
2026-03-29 21:18:36 +0800

    Creation of childclass (underGraduate) inherit from parent class (Student) and initiallization of super class attributes and childclass attributes through constructor(underGraduate).

--- src/main/java/scms/model/UnderGraduate.java
+++ src/main/java/scms/model/UnderGraduate.java
@@ -8,6 +8,27 @@ package scms.model;
  *
  * @author sumitchaudhary
  */
-public class UnderGraduate {
+public class UnderGraduate extends Student {
+    public String program;
+    public int duration;
+    public String adminName;
+    private int contactNo;
+    
+    public UnderGraduate(int studentID, String name, int age, String major, int semester, double percentageOfMarks,
+     double attendancePercentage, String program, int duration, String adminName, int contactNo){
+        super(studentID, name, age, major, semester, percentageOfMarks, attendancePercentage);
+        this.program = program;
+        this.duration = duration;
+        this.adminName = adminName;
+        this.contactNo = contactNo;
+    }
+    public String toString(){
+        return "Student [ ID = " + studentID +",Name = " + name + ",Major = " + 
+                major + ",Semester = " + semester + ",AttendancePercentage = " + 
+                attendancePercentage + ",PercentageOfMarks = " + percentageOfMarks + ",Progam = "
+                + program + ",Duration = " + duration + ",AdminName = " + adminName + "ContactNo = " 
+                + contactNo + "]";
+    }
+    
     
 }

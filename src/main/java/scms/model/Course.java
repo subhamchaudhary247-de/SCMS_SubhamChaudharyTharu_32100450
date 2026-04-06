a7c5386e62cea0397327c5f1fc8e1552210bc82e
SUMIT <sc259042@gmail.com>
2026-04-06 13:13:34 +0800

    Use of getter and setter methods for credits attribute in Course class.

--- src/main/java/scms/model/Course.java
+++ src/main/java/scms/model/Course.java
@@ -9,5 +9,30 @@ package scms.model;
  * @author sumitchaudhary
  */
 public class Course {
+    public String courseId;
+    public String courseName;
+    private int credits;
+    public String program;
+    
+    public Course( String courseId, String courseName, int credits, String program){
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.credits = credits;
+        this.program = program;
+    }
+    
+    public int getcredits(){
+        return credits;
+    }
+    
+    public void setcredits(int credits){
+        this.credits = credits;
+    }
+    
+    
+    public String toString(){
+       return(" Course [ ID = " + courseId + ", Name = " + courseName +
+                ", Credits = " + credits + ", Program = " + program + "]");
+    }
     
 }

1c273bd763dbc9342b1ca1bf5afa796fe569374e
SUMIT <sc259042@gmail.com>
2026-04-15 22:35:37 +0800

    Storing UnderGraduate and Postgraduate courses at course set.

--- src/main/java/scms/Main.java
+++ src/main/java/scms/Main.java
@@ -25,7 +25,7 @@ public class Main {
        Set<Course> course = new HashSet<>();
        Map<String,List<Student>> enrolled_std = new HashMap<>();
        
-       //Data of UnderGraduate Students
+       // Data of UnderGraduate Students
        UnderGraduate ug1 = new UnderGraduate(32100450, "Subham", 20, "CS", 3, 80, 85, "undergraduate", 3, "Tanmya Singha",+61-400-11111);
        UnderGraduate ug2 = new UnderGraduate(32100102, "Ujain", 19, "IT", 2, 75, 80, "undergraduate", 3, "Tanmya Singha", +61-400-111112);
        UnderGraduate ug3 = new UnderGraduate(32100103, "Mannu", 21, "CS", 4, 78, 82, "undergraduate", 3, "Tanmya Singha", +61-400-111113);
@@ -39,10 +39,10 @@ public class Main {
        UnderGraduate ug11 = new UnderGraduate(32100111, "Lucas", 22, "CS", 5, 83, 89, "undergraduate", 3, "Tanmya Singha", +61-400-111121);
        UnderGraduate ug12 = new UnderGraduate(32100112, "Mia", 18, "IT", 1, 72, 77, "undergraduate", 3, "Tanmya Singha", +61-400-111122);
        
-       //Adding data of UnderGraduate Students to the student List
+       // Adding data of UnderGraduate Students to the student List
        std.addAll(Arrays.asList(ug1, ug2, ug3, ug4, ug5, ug6, ug7, ug8, ug9, ug10));
        
-        //Data of Postgraduate Students
+        // Data of Postgraduate Students
         Postgraduate pg1 = new Postgraduate(201, "Alice", 24, "DS", 2, 88, 90, "postgraduate", 2, "Lisa Ban", +61-400-222221);
         Postgraduate pg2 = new Postgraduate(202, "David", 25, "Cyber", 1, 85, 87, "postgraduate", 2, "Lisa Ban", +61-400-222222);
         Postgraduate pg3 = new Postgraduate(203, "Chris", 26, "AI", 3, 90, 92, "postgraduate", 2, "Lisa Ban", +61-400-222223);
@@ -52,8 +52,29 @@ public class Main {
         Postgraduate pg7 = new Postgraduate(207, "Leo", 25, "ML", 3, 91, 93, "postgraduate", 2, "Lisa Ban", +61-400-222227);
         Postgraduate pg8 = new Postgraduate(208, "Anna", 23, "BigData", 1, 86, 88, "postgraduate", 2, "Lisa Ban", +61-400-222228);
        
-        //Adding data of Postraduate Students to the student List
+        // Adding data of Postraduate Students to the student List
         std.addAll(Arrays.asList(pg1, pg2, pg3, pg4, pg5, pg6, pg7, pg8));
+        
+        // UnderGraduate courses
+        Course c1 = new Course("COMP1005", "Object Oriented Programming", 60, "undergraduate");
+        Course c2 = new Course("COMP1004", "Mathematics of Computer Science", 60, "undergraduate");
+        Course c3 = new Course("COMP1006", "Artifical Intelligence", 60, "undergraduate");
+        Course c4 = new Course("COMP2014", "Networking", 60, "undergraduate");
+        Course c5 = new Course("COMP1001", "Principles of CyberSecurity", 60, "undergraduate");
+        
+        // Storing UnderGraduate courses at course set
+        course.addAll(Arrays.asList(c1, c2, c3, c4, c5));
+        
+        // Postgraduate courses
+        Course c6 = new Course("PG201", "Advanced Java", 60, "postgraduate");
+        Course c7 = new Course("PG202", "Machine Learning", 60, "postgraduate");
+        Course c8 = new Course("PG203", "Cloud", 60, "postgraduate");
+        Course c9 = new Course("PG204", "Web development", 60, "postgraduate");
+        Course c10 = new Course("PG205", "Data Science", 60, "postgraduate");
+        
+        // Storing Postgraduate courses at courses set
+        course.addAll(Arrays.asList(c6, c7, c8, c9, c10));
+        
        DashboardFrame dashboard = new DashboardFrame();
         dashboard.show();
     }
